5afe63a9444a92033a96ef4a2098248035ff489f
Joerg <joerg.heinze@monstec.de>
2019-05-07 15:55:07 +0200

    - resolved removed in cookie.js

--- src/js/chat.js
+++ src/js/chat.js
@@ -59,8 +59,7 @@ function initChatJs() {
 
         window.onmessage = ((e) => {
             if (e.data === 'sendPortToProduck' && allowedOrigins.includes(e.origin)) {
-                console.log(e);
-                new Promise((resolve) =>  resolve(sendCookieData()));
+                sendCookieData();
             }
         });
     }
