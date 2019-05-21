6123ec9f5e82c9c90c57ca5aeaa32865d95ee4b1
bk4mt <bk4mt@users.noreply.github.com>
2019-05-21 20:32:47 +0200

    increment version for 1.1.0 release

--- src/php/produck.php
+++ src/php/produck.php
@@ -34,7 +34,7 @@ class Produck extends Module
     {
         $this->name = 'produck';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.2';
+        $this->version = '1.1.0';
         $this->author = 'MonsTec UG haftungsbeschränkt';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
